41d940875e402d3619d58b44c6804100f464ff23
layvvs <mbmaximovich@mail.ru>
2023-04-18 00:49:47 +0300

    addaptive main menu

--- authapp/models.py
+++ authapp/models.py
@@ -4,4 +4,3 @@ from django.db import models
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=15, unique=False)
     email = models.EmailField(max_length=100, unique=True)
-    
