c86dea3cebede9d0165fac0264ed763bc92d1776
layvvs <mbmaximovich@mail.ru>
2023-06-04 14:33:03 +0300

    forms front ready

--- mainapp/static/js/forms.js
+++ mainapp/static/js/forms.js
@@ -5,10 +5,10 @@ function addAuthor(){
     authorCounter += 1;
     $(".new-authors-form").append(
         `<div class="mt-3 mb-1" id="author-${authorCounter}">
-            <input type="text" class="form-control" name=="authors-name-${authorCounter}" placeholder="ФИО" required>
-            <input type="text" class="form-control mt-1" name=="authors-unit-${authorCounter}" placeholder="Подразделение" required>
-            <input type="text" class="form-control mt-1" name=="authors-post-${authorCounter}" placeholder="Должность" required>
-            <button type="button" class="btn btn-primary mt-3" name=="remove-author-btn-${authorCounter}" onclick="removeAuthor(${authorCounter})">Удалить автора</button>
+            <input type="text" class="form-control" name="authors-name" placeholder="ФИО" required>
+            <input type="text" class="form-control mt-1" name="authors-unit" placeholder="Подразделение" required>
+            <input type="text" class="form-control mt-1" name="authors-post" placeholder="Должность" required>
+            <button type="button" class="btn btn-primary mt-3" name="remove-author-btn-${authorCounter}" onclick="removeAuthor(${authorCounter})">Удалить автора</button>
         </div>`
         );
 }
@@ -17,7 +17,7 @@ function addSecurity(){
     securityCounter += 1;
     $(".new-security-form").append(
         `<div class="mt-3 mb-1" id="security-${securityCounter}">
-            <input type="text" class="form-control mt-1" id="security-name-${securityCounter}" required>
+            <input type="text" class="form-control mt-1" name="security-abroad" required>
             <button type="button" class="btn btn-primary mt-3" id="remove-author-btn-${securityCounter}" onclick="removeSecurity(${securityCounter})">Удалить поле</button>
         </div>`
         );
@@ -37,13 +37,13 @@ $("#select-service-proactive").on('click', function(ev){
     if ($('#select-service-proactive').val() == "Служебный"){
         $('#ser-proac-forms').replaceWith(
             '<div class="mt-3" id="ser-proac-forms">\
-                <select class="form-control custom-select mt-1" id="exampleFormControlSelect1" required>\
+                <select class="form-control custom-select mt-1" name="select-type-service" required>\
                     <option selected>Распоряжение</option>\
                     <option>Приказ</option>\
                     <option>Служебное задание</option>\
                 </select>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Номер приказа" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Номер договора" required>\
+                <input type="text" class="form-control mt-1" name="order-number" placeholder="Номер приказа" required>\
+                <input type="text" class="form-control mt-1" name="contract-number" placeholder="Номер договора" required>\
             </div>'
 
         );
@@ -51,8 +51,8 @@ $("#select-service-proactive").on('click', function(ev){
     else{
         $('#ser-proac-forms').replaceWith(
             '<div class="mt-3" id="ser-proac-forms">\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Номер договора" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Дата договора" required>\
+                <input type="text" class="form-control mt-1" name="contract-number" placeholder="Номер договора" required>\
+                <input type="text" class="form-control mt-1" name="contract-date" placeholder="Дата договора" required>\
             </div>'
         );
     }
@@ -63,19 +63,19 @@ $("#type-of-contract").on('click', function(ev){
     if ($('#type-of-contract').val() == "Лицензионный договор"){
         $('#type-of-contract-forms').replaceWith(
             '<div class="mt-3" id="type-of-contract-forms">\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Дата заключения" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Номер" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Дата регистрации" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Срок оплаты" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Оплата" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Дата оплаты" required>\
+                <input type="text" class="form-control mt-1" name="date-of-conclusion" placeholder="Дата заключения" required>\
+                <input type="text" class="form-control mt-1" name="number" placeholder="Номер" required>\
+                <input type="text" class="form-control mt-1" name="date-of-registration" placeholder="Дата регистрации" required>\
+                <input type="text" class="form-control mt-1" name="due-date" placeholder="Срок оплаты" required>\
+                <input type="text" class="form-control mt-1" name="payment" placeholder="Оплата" required>\
+                <input type="text" class="form-control mt-1" name="date-of-payment" placeholder="Дата оплаты" required>\
                 <label for="license-agreement-1">Использование в уставном капитале</label> <br>\
                 <div class="form-check form-check-inline">\
-                    <input class="form-check-input" type="radio" name="license-agreement" id="license-agreement-1" value="option1" checked>\
+                    <input class="form-check-input" type="radio" name="license-agreement" id="license-agreement-1" value="Да" checked>\
                     <label class="form-check-label" for="license-agreement-1">Да</label>\
                 </div>\
                 <div class="form-check form-check-inline">\
-                    <input class="form-check-input" type="radio" name="license-agreement" id="license-agreement-2" value="option2">\
+                    <input class="form-check-input" type="radio" name="license-agreement" id="license-agreement-2" value="Нет">\
                     <label class="form-check-label" for="license-agreement-2">Нет</label>\
                 </div>\
             </div>'
@@ -84,11 +84,11 @@ $("#type-of-contract").on('click', function(ev){
     else if ($('#type-of-contract').val() == "Договор отчуждения"){
         $('#type-of-contract-forms').replaceWith(
             '<div class="mt-3" id="type-of-contract-forms">\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Дата заключения" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Номер" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Дата регистрации" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Оплата" required>\
-                <input type="text" class="form-control mt-1" id="formGroupExampleInput2" placeholder="Дата оплаты" required>\
+                <input type="text" class="form-control mt-1" name="date-of-conclusion" placeholder="Дата заключения" required>\
+                <input type="text" class="form-control mt-1" name="number" placeholder="Номер" required>\
+                <input type="text" class="form-control mt-1" name="date-of-registration" placeholder="Дата регистрации" required>\
+                <input type="text" class="form-control mt-1" name="payment" placeholder="Оплата" required>\
+                <input type="text" class="form-control mt-1" name="date-of-payment" placeholder="Дата оплаты" required>\
             </div>'
         );
     }
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
+
 
 def main_index(request: HttpRequest):
     counter = IpcArchive.objects.count()
@@ -10,5 +12,8 @@ def main_index(request: HttpRequest):
     return render(request, 'mainapp/main-index.html', context)
 
 def new_doc(request: HttpRequest):
+    if request.method == "POST":
+        print(request.POST)
+        messages.success(request, 'Документ успешно добавлен в базу.')    
+        return redirect('/')
     return render(request, 'mainapp/new-doc.html')
-
